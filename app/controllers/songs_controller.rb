00139b2d7eda13eb942ba7ea33ce491146c979b0
Obafemi Fadairo <ofpupp@princeton.edu>
2018-09-06 23:21:14 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,6 +26,7 @@ class SongsController < ApplicationController
 
   def update
     @song = Song.find(params[:id])
+    binding.pry
     @song.update(song_params)
     redirect_to song_path(@song)
   end
